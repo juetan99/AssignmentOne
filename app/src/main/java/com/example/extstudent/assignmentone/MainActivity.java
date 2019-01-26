bddd0c9503e95274b1eb7f315602d390c9c3ac43
juetan99 <juetan99@yahoo.com>
2019-01-26 13:57:30 -0800

    button added

--- app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.extstudent.assignmentone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String firstName;
+    private String lastName;
+
+    private TextView firstNameTextView = null;
+    private TextView lastNameTextView = null;
+    private Button submitButton = null;
+    private TextView resultTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.firstNameTextView= (TextView)findViewById(R.id.editTextFirstName);
+        this.lastNameTextView = (TextView)findViewById(R.id.editTextLastName);
+        this.resultTextView= (TextView)findViewById(R.id.textViewResult);
+        
+
+
     }
 }
